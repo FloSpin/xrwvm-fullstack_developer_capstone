bf1143696dfcbedb06db55f96ecb4ec0ddae170a
flospin <florianspinoza@gmail.com>
2024-11-06 04:03:00 -0500

    w5 lint py restapi 2

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -23,6 +23,7 @@ def get_request(endpoint, **kwargs):
         return response.json()
     except Exception as err:
         #If any error occurs
+        print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
 def analyze_review_sentiments(text):
@@ -36,10 +37,11 @@ def analyze_review_sentiments(text):
         print("Network exception occurred")
 
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
